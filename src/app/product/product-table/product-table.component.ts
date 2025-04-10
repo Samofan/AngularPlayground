c14831eb660128b6d1d5d49a9402fd291b08a5d5
Samofan <dinter.florian@googlemail.com>
2025-04-10 21:17:05 +0200

    feat: :sparkles: Add products table; implement filter by category

--- src/app/product/product-table/product-table.component.ts
+++ src/app/product/product-table/product-table.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Product } from '@/shared';
+import { Component, input, InputSignal } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { Tag } from 'primeng/tag';
 
 @Component({
   selector: 'app-product-table',
-  imports: [],
+  imports: [TableModule, Tag],
   templateUrl: './product-table.component.html',
   styleUrl: './product-table.component.scss'
 })
 export class ProductTableComponent {
-
+  products: InputSignal<Product[]> = input.required<Product[]>();
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,29 +1,44 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { CategorySelectComponent } from "./category-select/category-select.component";
+import { CategorySelectComponent } from './category-select/category-select.component';
 import { ProductTableComponent } from './product-table/product-table.component';
 import { Category, Product, ProductService } from '@/shared';
-import { subscribeToArray } from 'rxjs/internal/util/subscribeToArray';
 
 @Component({
   selector: 'app-product',
   imports: [CategorySelectComponent, ProductTableComponent],
   templateUrl: './product.component.html',
-  styleUrl: './product.component.scss'
+  styleUrl: './product.component.scss',
 })
 export class ProductComponent implements OnInit {
   private readonly productService = inject(ProductService);
 
   availableCategories: Category[] = [];
+  selectedCategories: Category[] = [];
   products: Product[] = [];
 
   ngOnInit(): void {
-    this.productService.getProducts().subscribe(products => {
+    this.productService.getProducts().subscribe((products) => {
       this.products = products;
-      this.availableCategories = this.productService.getDistinctCategoriesOfProducts(this.products);
+      this.availableCategories =
+        this.productService.getDistinctCategoriesOfProducts(this.products);
     });
   }
 
-  onCategorySelected(selectedCategories: any): void {
-    console.log('Selected categories:', selectedCategories);
+  onCategorySelected(selectedCategories: Category[]): void {
+    this.selectedCategories = selectedCategories;
+
+    if (this.selectedCategories.length === 0) {
+      this.productService.getProducts().subscribe((products) => {
+        this.products = products;
+      });
+    } else {
+      this.productService.getProducts().subscribe((products) => {
+        this.products = products.filter((product) =>
+          this.selectedCategories.some(
+            (category) => category === product.category
+          )
+        );
+      });
+    }
   }
 }
